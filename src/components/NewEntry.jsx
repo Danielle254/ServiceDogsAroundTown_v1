3abf68842bccd1c27be4a2c3250ac19575a8b637
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-14 09:05:53 -0600

    add components

---
+++ src/components/NewEntry.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function NewEntry() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/OrganizeList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OrganizeList() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
