ccce05041464f75947febd93c190bdea9cdbeada
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-15 11:14:24 -0600

    create add and delete entry functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {React, useState}  from 'react'
+import {React, useEffect, useState}  from 'react'
 import Banner from './components/Banner'
 import NewEntry from './components/NewEntry'
 import OrganizeList from './components/OrganizeList'
@@ -9,6 +9,24 @@ import Footer from './components/Footer'
 
 function App() {
   const [sortBy, setSortBy] = useState('date');
+  const [data, setData] = useState([]);
+
+  // retrieve any stored user entries from local storage upon initial app load
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem('SDAT_data'));
+    if (storedData) {
+      setData(storedData);
+    }
+  }, []);
+
+  // update local storage when list of entries changes
+  useEffect(() => {
+    localStorage.setItem('SDAT_data', JSON.stringify(data));
+  }, [data]);
+
+  function addEntry (entry) {
+    setData([...data, entry]);    
+  }
 
   function changeSort () {
     if (sortBy === "date") {
@@ -18,17 +36,22 @@ function App() {
     }
   }
 
+  
+
   return (
     <div className='App'>
       <Banner />
       <main>
-        <NewEntry />
+        <NewEntry 
+        addEntry={addEntry}
+        />
         <OrganizeList 
         sortBy={sortBy}
         changeSort={changeSort}        
         />
         <EntryList 
         sortBy={sortBy}
+        data={data}
         />
       </main>
       <Footer />
