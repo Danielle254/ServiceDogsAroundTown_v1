2786fba75b40096b6792a266b0b0600505ec2219
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-15 11:16:10 -0600

    create entry component

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Entry() {
+export default function Entry(props) {
+  
+
   return (
-    <div>
-      
+    <div className='entry'>
+      <h3>{props.name}</h3>
+      <p>{props.isFavorite ? "❤️" : "🤍"}</p>
+      <p>{props.personalNote}</p>
+      <button>Edit</button>
+      <button onClick={props.deleteEntry(props.id)}>Delete</button>
     </div>
   )
 }
