3be2613b845a3577df9dd9957a000687b125f7bf
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-15 13:35:42 -0600

    display nothing if not favorite and adjust delete button function

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 export default function Entry(props) {
-  
+  const id = props.id;
 
   return (
     <div className='entry'>
       <h3>{props.name}</h3>
-      <p>{props.isFavorite ? "❤️" : "🤍"}</p>
+      <p>{props.isFavorite ? "❤️" : ""}</p>
       <p>{props.personalNote}</p>
       <button>Edit</button>
-      <button /* onClick={() => props.deleteEntry(props.id)} */>Delete</button>
+      <button onClick={() => props.deleteEntry(id)}>Delete</button>
     </div>
   )
 }
