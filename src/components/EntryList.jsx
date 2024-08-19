2fd3f58b517097e55998b63b7af8a2bbc78937b8
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-19 12:39:20 -0600

    update to utilize favorites filter

--- src/components/EntryList.jsx
+++ src/components/EntryList.jsx
@@ -2,24 +2,43 @@ import React from 'react'
 import Entry from './Entry'
 
 export default function EntryList(props) {
+  let entries;
   
-  const entries = props.data.map(
-    entry => {
-      return (
-        <Entry 
-        name={entry.name}
-        location={entry.location}
-        dateVisited={entry.dateVisited}
-        isFavorite={entry.isFavorite}
-        personalNote={entry.personalNote}
-        id={entry.id}
-        key={entry.id}
-        deleteEntry={props.deleteEntry}
-        />
-      )
-    }
-  )
-
+  if (props.filterFavorites) {
+    entries = props.data.filter((each) => each.isFavorite).map(
+      entry => {
+        return (
+          <Entry 
+          name={entry.name}
+          location={entry.location}
+          dateVisited={entry.dateVisited}
+          isFavorite={entry.isFavorite}
+          personalNote={entry.personalNote}
+          id={entry.id}
+          key={entry.id}
+          deleteEntry={props.deleteEntry}
+          />
+        )
+      }
+    )
+  } else {
+    entries = props.data.map(
+      entry => {
+        return (
+          <Entry 
+          name={entry.name}
+          location={entry.location}
+          dateVisited={entry.dateVisited}
+          isFavorite={entry.isFavorite}
+          personalNote={entry.personalNote}
+          id={entry.id}
+          key={entry.id}
+          deleteEntry={props.deleteEntry}
+          />
+        )
+      }
+    )
+  }
   
   
   return (
