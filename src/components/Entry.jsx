6a36a4102f3d0b773c0fb5749002ed1a8ec3b2b0
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-19 10:31:57 -0600

    render location

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -6,6 +6,7 @@ export default function Entry(props) {
   return (
     <div className='entry'>
       <h3>{props.name} <span>{props.isFavorite ? "❤️" : ""}</span></h3>
+      <p>{props.location}</p>
       <p>{props.personalNote}</p>
       <button className='entry--button'>Edit</button>
       <button className='entry--button' onClick={() => props.deleteEntry(id)}>Delete</button>
