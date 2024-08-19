6d019267fb821daa883ce97a03493595203f4e44
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-19 11:48:45 -0600

    render date visited to entry card

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 export default function Entry(props) {
   const id = props.id;
+  
 
   return (
     <div className='entry'>
       <h3>{props.name} <span>{props.isFavorite ? "❤️" : ""}</span></h3>
       <p>{props.location}</p>
+      <p>Visited: {props.dateVisited}</p>
       <p>{props.personalNote}</p>
       <button className='entry--button'>Edit</button>
       <button className='entry--button' onClick={() => props.deleteEntry(id)}>Delete</button>
