e7dd91b147e0b654f2fb8d4a9b87268d11e7b3ca
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-19 11:22:47 -0600

    update to implement full width background

--- src/components/OrganizeList.jsx
+++ src/components/OrganizeList.jsx
@@ -4,15 +4,17 @@ export default function OrganizeList(props) {
   
   
   return (
-    <div className='nav'>
-      <div className='sort' >
-        <p>Sort By:</p>
-        <button className='sort--button' onClick={props.changeSort}>Date</button>
-        <button className='sort--button' onClick={props.changeSort}>Business Name</button>
-      </div>
-      <div className='filter'>
-        <p>Filter By:</p>
-        <button className='filter--button' onClick={props.filter}>Favorites ❤️</button>
+    <div className='nav--background'>
+      <div className='nav'>
+        <div className='sort' >
+          <p>Sort By:</p>
+          <button className='sort--button' onClick={props.changeSort}>Date</button>
+          <button className='sort--button' onClick={props.changeSort}>Business Name</button>
+        </div>
+        <div className='filter'>
+          <p>Filter By:</p>
+          <button className='filter--button' onClick={props.filter}>Favorites ❤️</button>
+        </div>
       </div>
     </div>
   )
