ad83ce5194f4f5c11a64a4dfa055bd6f4c15314f
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-19 12:00:30 -0600

    created selected classname for sort button

--- src/components/OrganizeList.jsx
+++ src/components/OrganizeList.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 
 export default function OrganizeList(props) {
-  
+  let dateButton, nameButton = "";
+  if (props.sortBy === "date") {
+    dateButton = 'sort--button selected';
+    nameButton = 'sort--button';
+  }
+  if (props.sortBy === "name") {
+    dateButton = 'sort--button';
+    nameButton = 'sort--button selected';
+  }
   
   return (
     <div className='nav--background'>
       <div className='nav'>
         <div className='sort' >
           <p>Sort By:</p>
-          <button className='sort--button' onClick={props.changeSort}>Date</button>
-          <button className='sort--button' onClick={props.changeSort}>Business Name</button>
+          <button className={dateButton} onClick={props.changeSort}>Date Visited</button>
+          <button className={nameButton} onClick={props.changeSort}>Business Name</button>
         </div>
         <div className='filter'>
           <p>Filter By:</p>
