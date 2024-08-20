b463baf4d871825821731139e7fa58eb72693912
Danielle Lindblom <danielle.lindblom@gmail.com>
2024-08-20 08:40:43 -0600

    update banner to add image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Banner() {
   return (
     <header>
-      <h1>Service Dogs Around Town 🐕‍🦺</h1>
+      <h1>Service Dogs <br/>Around Town</h1>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/b/bf/Service_dogs_20181212-Library-52_%2846753606291%29.jpg' alt='yellow labrador retriever service dog wearing a green guide dogs for the blind vest looks up and to the right while laying down'/>
     </header>
   )
 }
